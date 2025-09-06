f6b55af5ba9e11643595cf894059f58ec8dc6c33
Lee-Home <charismafight@hotmail.com>
2025-09-07 03:52:11 +0800

    upload main file

---
+++ main.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         jp kawai
+// @namespace    charismafight@hotmail.com
+// @version      0.1
+// @description  修改网页标题为“Hello, Tampermonkey!”
+// @author       lli
+// @match        https://www.tokyokawaiilife.jp/*
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  const testName = "カートに入れる";
+  document.addEventListener("click", (e) => {
+    if (e.target.matches(`input[alt="${testName}"]`)) {
+      console.log(e.target);
+    }
+  });
+})();
